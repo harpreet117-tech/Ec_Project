c30ee7aa9dc5cd02f1af5de03a31e3dd1f0f9b28
harpreet117-tech <balkarnmann140@gmail.com>
2026-04-15 18:48:40 -0600

    Update App.js with group info

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 test('renders group and names', () => {
   render(<App />);
   
-  expect(screen.getByText(/Group 5/i)).toBeInTheDocument();
+  expect(screen.getByText(/Group 6/i)).toBeInTheDocument();
   expect(screen.getByText(/Harpreet Singh/i)).toBeInTheDocument();
 });
