2ca0efd1ad666425d44973601341c3c30e2bc180
Harpreet Singh <balkarnmann140@gmail.com>
2026-04-20 12:36:59 -0600

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -6,4 +6,6 @@ test('renders group and names', () => {
   
   expect(screen.getByText(/Group 6/i)).toBeInTheDocument();
   expect(screen.getByText(/Harpreet Singh/i)).toBeInTheDocument();
+  expect(screen.getByText(/Virendra Gadhavi/i)).toBeInTheDocument();
+  expect(screen.getByText(/Saeed Awais/i)).toBeInTheDocument();
 });
